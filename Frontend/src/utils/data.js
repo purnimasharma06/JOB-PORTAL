d5ce58d32fef47a3c21cc36c2ad4f7d2e477f239
purnimasharma06 <purnima.sharma3773@gmail.com>
2026-09-01 13:57:06 +0530

    updated

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,6 @@
-export const USER_API_ENDPOINT = "http://localhost:8000/api/user";
-export const JOB_API_ENDPOINT = "http://localhost:8000/api/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:8000/api/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:8000/api/company";
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const USER_API_ENDPOINT = `${API_URL}/api/user`;
+export const JOB_API_ENDPOINT = `${API_URL}/api/job`;
+export const APPLICATION_API_ENDPOINT = `${API_URL}/api/application`;
+export const COMPANY_API_ENDPOINT = `${API_URL}/api/company`;
